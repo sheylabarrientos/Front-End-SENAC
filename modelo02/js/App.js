cd4c55669eed5e3666153438721afe9cd6556183
Sheyla Barrientos <60662105+sheylabarrientos@users.noreply.github.com>
2021-06-11 10:18:30 -0300

    Delete modelo02 directory

--- modelo02/js/App.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    var formulario = document.getElementById("formulario");
-
-    var kilos = formulario.kilos.value
-    var metros = formulario.metros.value
-    var centimetros = formulario.centimetros.value
-
-    alert("Deu certo");
-}
